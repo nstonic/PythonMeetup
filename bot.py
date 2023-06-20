bae4422e6e27bb9e38eb32a4e864981da86a5399
nstonic <118763806+nstonic@users.noreply.github.com>
2023-06-20 22:52:59 +0400

    Add speech_list and future_events

--- bot.py
+++ bot.py
@@ -33,8 +33,7 @@ def handle_main_menu(update, context):
         query,
         partial(show_event, event_id=query)
     )
-    print(action)
-    action(update, context)
+    return action(update, context)
 
 
 def handle_event_menu(update, context):
@@ -42,7 +41,7 @@ def handle_event_menu(update, context):
     event_id = context.user_data['current_event']
     actions = {
         'speech_list': partial(show_speech_list, event_id=event_id),
-        'main_menu': show_start_menu,
+        'back': show_start_menu,
         'register': partial(register, event_id=event_id),
         'ask': ask,
         'meet': meet,
@@ -50,7 +49,23 @@ def handle_event_menu(update, context):
         'donate': partial(donate, event_id=event_id)
     }
     action = actions.get(query)
-    action(update, context)
+    return action(update, context)
+
+
+def handle_future_events(update, context):
+    query = update.callback_query.data
+    if query == 'back':
+        return show_start_menu(update, context)
+    else:
+        event_id = context.user_data['current_event']
+        return show_event(update, context, event_id)
+
+
+def handle_speech_list_menu(update, context):
+    query = update.callback_query.data
+    event_id = context.user_data.get('current_event')
+    if query == 'back':
+        return show_event(update, context, event_id)
 
 
 def handle_users_reply(update, context):
@@ -68,16 +83,15 @@ def handle_users_reply(update, context):
     state_functions = {
         'START': show_start_menu,
         'HANDLE_MAIN_MENU': handle_main_menu,
-        'HANDLE_EVENT_MENU': handle_event_menu
+        'HANDLE_EVENT_MENU': handle_event_menu,
+        'HANDLE_FUTURE_EVENTS': handle_future_events,
+        'HANDLE_SPEECH_LIST_MENU': handle_speech_list_menu
     }
-
     state_handler = state_functions.get(user_state, show_start_menu)
-
     next_state = state_handler(
         update=update,
         context=context
     )
-
     context.user_data['state'] = next_state
 
 
--- bot_lib.py
+++ bot_lib.py
@@ -1,9 +1,52 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from contextlib import suppress
+
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, TelegramError
+
+
+def answer_to_user(
+        update,
+        context,
+        text,
+        keyboard=None,
+        add_back_button: bool = True,
+        parse_mode: str = 'HTML',
+        edit_current_message: bool = True
+):
+    if add_back_button:
+        keyboard.append(
+            [InlineKeyboardButton('Назад', callback_data='back')]
+        )
+
+    if edit_current_message:
+        try:
+            message = context.bot.edit_message_text(
+                chat_id=update.effective_chat.id,
+                message_id=update.effective_message.message_id,
+                text=text,
+                reply_markup=InlineKeyboardMarkup(keyboard),
+                parse_mode=parse_mode
+            )
+        except TelegramError:
+            pass
+        else:
+            return message
+
+    with suppress(TelegramError):
+        context.bot.delete_message(
+            chat_id=update.effective_chat.id,
+            message_id=update.effective_message.message_id
+        )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        reply_markup=InlineKeyboardMarkup(keyboard),
+        parse_mode=parse_mode
+    )
 
 
 def show_start_menu(update, context):
     user_id = update.effective_chat.id
-    event_id = 1  # TODO Ищем мероприятие которое сейчас проходит, если нет, то ближайшее, которое ожидается
+    event_id = 1  # TODO Ищем мероприятие, которое сейчас проходит. Если нет, то ближайшее, которое ожидается
     text = 'Добро пожаловать в бот PythonMeetup'
     keyboard = [
         [InlineKeyboardButton('Ближайшее мероприятие', callback_data=event_id)],
@@ -14,10 +57,12 @@ def show_start_menu(update, context):
             [InlineKeyboardButton('Создать мероприятие', callback_data='create_event')]
         )
 
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
+    answer_to_user(
+        update,
+        context,
         text=text,
-        reply_markup=InlineKeyboardMarkup(keyboard)
+        keyboard=keyboard,
+        add_back_button=False
     )
 
     return 'HANDLE_MAIN_MENU'
@@ -34,44 +79,45 @@ def show_event(update, context, event_id):
     user = None  # TODO получаем данные о пользователе
 
     keyboard = [
-        [InlineKeyboardButton('Расписание выступлений', callback_data='speech_list')],
-        [InlineKeyboardButton('Назад', callback_data='main_menu')]
+        [InlineKeyboardButton('Расписание выступлений', callback_data='speech_list')]
     ]
     if not user:  # TODO Проверяем, что пользователь не зарегистрирован как участник данного мероприятия
-        keyboard.insert(
-            1,
+        keyboard.append(
             [InlineKeyboardButton('Регистрация', callback_data='register')]
         )
     else:
         if True:  # TODO Проверяем, что мероприятие проходит в данный момент
-            keyboard.insert(
-                1,
+            keyboard.append(
                 [InlineKeyboardButton('Задать вопрос', callback_data='ask'),
                  InlineKeyboardButton('Познакомиться', callback_data='meet')]
             )
 
     if user:  # TODO Проверяем, является ли пользователь организатором данного мероприятия
-        keyboard.insert(
-            2,
+        keyboard.append(
             [InlineKeyboardButton('Редактировать', callback_data='edit')]
         )
     else:
-        keyboard.insert(
-            2,
+        keyboard.append(
             [InlineKeyboardButton('Задонатить', callback_data='donate')]
         )
 
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
+    answer_to_user(
+        update,
+        context,
         text=f'<b>{event_title}</b>\n\n{event_text}',
-        reply_markup=InlineKeyboardMarkup(keyboard),
-        parse_mode='HTML'
+        keyboard=keyboard
     )
     return 'HANDLE_EVENT_MENU'
 
 
 def show_speech_list(update, context, event_id):
-    pass
+    speech_list = []  # TODO получаем данные о выступлениях на данном мероприятии
+    answer_to_user(
+        update,
+        context,
+        text='\n'.join(speech_list)
+    )
+    return 'HANDLE_SPEECH_LIST_MENU'
 
 
 def register(update, context, event_id):
@@ -95,7 +141,24 @@ def donate(update, context, event_id):
 
 
 def show_future_events(update, context):
-    pass
+    events = []  # TODO получаем список грядущих мероприятий
+    keyboard = None
+    if events:
+        text = 'Вот какие мероприятия пройдут в скором времени'
+        keyboard = [
+            [InlineKeyboardButton(event.title, callback_data=event.pk)]
+            for event in events
+        ]
+    else:
+        text = 'К сожалению в ближайшее время мероприятий не ожидается'
+
+    answer_to_user(
+        update,
+        context,
+        text=text,
+        keyboard=keyboard
+    )
+    return 'HANDLE_FUTURE_EVENTS'
 
 
 def create_event(update, context):

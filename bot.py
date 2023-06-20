6c404b7f3b253c038328d29a7b28386fbb25cddc
nstonic <118763806+nstonic@users.noreply.github.com>
2023-06-20 21:37:44 +0400

    Realize main and event menu

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from environs import Env
 from telegram.ext import (
     Updater,
@@ -7,17 +9,51 @@ from telegram.ext import (
     Filters
 )
 
-from bot_lib import show_events, show_start_menu
+from bot_lib import (
+    show_future_events,
+    show_start_menu,
+    create_event,
+    show_event,
+    show_speech_list,
+    register,
+    ask,
+    meet,
+    edit,
+    donate
+)
 
 
 def handle_main_menu(update, context):
     query = update.callback_query.data
-    if query == 'events':
-        return show_events(update, context)
+    actions = {
+        'future_events': show_future_events,
+        'create_event': create_event
+    }
+    action = actions.get(
+        query,
+        partial(show_event, event_id=query)
+    )
+    print(action)
+    action(update, context)
+
+
+def handle_event_menu(update, context):
+    query = update.callback_query.data
+    event_id = context.user_data['current_event']
+    actions = {
+        'speech_list': partial(show_speech_list, event_id=event_id),
+        'main_menu': show_start_menu,
+        'register': partial(register, event_id=event_id),
+        'ask': ask,
+        'meet': meet,
+        'edit': partial(edit, event_id=event_id),
+        'donate': partial(donate, event_id=event_id)
+    }
+    action = actions.get(query)
+    action(update, context)
 
 
 def handle_users_reply(update, context):
-
     if update.message:
         user_reply = update.message.text
     elif update.callback_query:
@@ -29,16 +65,19 @@ def handle_users_reply(update, context):
         user_state = 'START'
     else:
         user_state = context.user_data.get('state')
-
     state_functions = {
-        'START': show_start_menu
+        'START': show_start_menu,
+        'HANDLE_MAIN_MENU': handle_main_menu,
+        'HANDLE_EVENT_MENU': handle_event_menu
     }
 
     state_handler = state_functions.get(user_state, show_start_menu)
+
     next_state = state_handler(
         update=update,
         context=context
-    ) or user_state
+    )
+
     context.user_data['state'] = next_state
 
 
--- bot_lib.py
+++ bot_lib.py
@@ -2,10 +2,18 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def show_start_menu(update, context):
-    text = 'Основное меню'
+    user_id = update.effective_chat.id
+    event_id = 1  # TODO Ищем мероприятие которое сейчас проходит, если нет, то ближайшее, которое ожидается
+    text = 'Добро пожаловать в бот PythonMeetup'
     keyboard = [
-        [InlineKeyboardButton('Расписание мероприятий', callback_data='events')],
+        [InlineKeyboardButton('Ближайшее мероприятие', callback_data=event_id)],
+        [InlineKeyboardButton('Расписание мероприятий', callback_data='future_events')]
     ]
+    if user_id:  # TODO Сюда вставить проверку является ли пользователь админом
+        keyboard.append(
+            [InlineKeyboardButton('Создать мероприятие', callback_data='create_event')]
+        )
+
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=text,
@@ -15,5 +23,80 @@ def show_start_menu(update, context):
     return 'HANDLE_MAIN_MENU'
 
 
-def show_events(update, context):
+def show_event(update, context, event_id):
+    context.user_data['current_event'] = event_id
+
+    event = None  # TODO получаем данные о мероприятии
+    event_title = 'Учим python-telegram-bot'
+    event_text = 'Сильно учим'
+
+    user_id = update.effective_chat.id
+    user = None  # TODO получаем данные о пользователе
+
+    keyboard = [
+        [InlineKeyboardButton('Расписание выступлений', callback_data='speech_list')],
+        [InlineKeyboardButton('Назад', callback_data='main_menu')]
+    ]
+    if not user:  # TODO Проверяем, что пользователь не зарегистрирован как участник данного мероприятия
+        keyboard.insert(
+            1,
+            [InlineKeyboardButton('Регистрация', callback_data='register')]
+        )
+    else:
+        if True:  # TODO Проверяем, что мероприятие проходит в данный момент
+            keyboard.insert(
+                1,
+                [InlineKeyboardButton('Задать вопрос', callback_data='ask'),
+                 InlineKeyboardButton('Познакомиться', callback_data='meet')]
+            )
+
+    if user:  # TODO Проверяем, является ли пользователь организатором данного мероприятия
+        keyboard.insert(
+            2,
+            [InlineKeyboardButton('Редактировать', callback_data='edit')]
+        )
+    else:
+        keyboard.insert(
+            2,
+            [InlineKeyboardButton('Задонатить', callback_data='donate')]
+        )
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'<b>{event_title}</b>\n\n{event_text}',
+        reply_markup=InlineKeyboardMarkup(keyboard),
+        parse_mode='HTML'
+    )
+    return 'HANDLE_EVENT_MENU'
+
+
+def show_speech_list(update, context, event_id):
+    pass
+
+
+def register(update, context, event_id):
+    pass
+
+
+def ask(update, context):
+    pass
+
+
+def meet(update, context):
+    pass
+
+
+def edit(update, context, event_id):
+    pass
+
+
+def donate(update, context, event_id):
+    pass
+
+
+def show_future_events(update, context):
+    pass
+
+
+def create_event(update, context):
     pass

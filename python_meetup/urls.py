9f2e3b9cdb3c8c1ad6938c1b9b11d02b9ee2e2ec
nstonic <nstonic@gmail.com>
2023-06-22 09:15:37 +0400

    Fix redirection

--- python_meetup/urls.py
+++ python_meetup/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import tg_bot.views
+
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', tg_bot.views.admin),
     path('admin/', admin.site.urls),
 ]

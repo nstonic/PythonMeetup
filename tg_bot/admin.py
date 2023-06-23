ba2765dd72c1c6ac195860e1f516c10af56436f1
nstonic <118763806+nstonic@users.noreply.github.com>
2023-06-23 19:54:23 +0400

    Add image to admin

--- tg_bot/admin.py
+++ tg_bot/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import User, Event, Speech
 
@@ -14,7 +15,7 @@ class OrganizerInline(admin.TabularInline):
     verbose_name_plural = 'Организаторы'
 
 
-class ParticipantInlaine(admin.TabularInline):
+class ParticipantInline(admin.TabularInline):
     model = User.events.through
     extra = 1
     verbose_name_plural = 'Участники'
@@ -23,8 +24,15 @@ class ParticipantInlaine(admin.TabularInline):
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'started_at', 'get_organizer')
-    fields = ('title', 'description', 'started_at', 'finished_at')
-    inlines = [OrganizerInline, ParticipantInlaine]
+    readonly_fields = ['get_image_preview']
+    fields = ('get_image_preview', 'image', 'title', 'description', 'started_at', 'finished_at')
+    inlines = [OrganizerInline, ParticipantInline]
+
+    def get_image_preview(self, obj):
+        img = obj.image
+        if not img:
+            return 'выберите картинку'
+        return format_html('<img src="{url}" style="max-height: 200px;"/>', url=img.url)
 
 
 admin.site.register(Speech)
